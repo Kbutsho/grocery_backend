be77c732a84385cf939dd6381ffd563bd9ed7db0
Kaushik Biswas <kbutsho@gmail.com>
2022-12-01 22:20:12 +0600

    added

---
+++ routes/userRoute.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const {
+  registerUser,
+  loginUser,
+  logout,
+  getUser,
+  loginStatus,
+  updateUser,
+  changePassword,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/userController");
+const protect = require("../middleWare/authMiddleware");
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logout);
+router.get("/getUser", protect, getUser);
+router.get("/loggedIn", loginStatus);
+router.patch("/updateUser", protect, updateUser);
+router.patch("/changePassword", protect, changePassword);
+router.post("/forgotPassword", forgotPassword);
+router.put("/resetpassword/:resetToken", resetPassword);
+
+module.exports = router;
